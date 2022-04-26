79259c11d8192ffa4bec60b787e27baaec1bb52e
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:16:41 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part stock_shipment_out_alternative_reports module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_stock_shipment_out_alternative_reports import suite
-
-__all__ = ['suite']
